82bfb54c991bdd13d6222890fd97a59a71c6cafd
Iiro Hietala <iiro.hietala@descom.fi>
2014-05-22 18:14:14 +0300

    Added pom.xml

---
+++ testing/src/main/java/org/silverduck/jacetesting/services/tool/ToolService.java
@@ -0,0 +1,18 @@
+package org.silverduck.jacetesting.services.tool;
+
+import org.silverduck.jacetesting.domain.tool.Wrench;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Created by ihietala on 22.5.2014.
+ */
+@Stateless
+@LocalBean
+public class ToolService {
+    public void wrench(Wrench wrench) {
+        System.out.println("Wrenching away with my " + wrench);
+    }
+
+}
