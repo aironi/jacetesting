f57e2462befa8c16b3f7fef1cb5bfb53ff5e3a41
Iiro Hietala <iiro.hietala@descom.fi>
2014-05-30 00:40:56 +0300

    Jace #1: Test commit with pattern

--- testing/src/main/java/org/silverduck/jacetesting/domain/tool/CSpanner.java
+++ testing/src/main/java/org/silverduck/jacetesting/domain/tool/CSpanner.java
@@ -4,13 +4,13 @@ package org.silverduck.jacetesting.domain.tool;
  * Created by ihietala on 29.5.2014.
  */
 public class CSpanner extends AbstractTool {
-    private long sizeInMm;
+    private long size;
 
-    public long getSizeInMm() {
-        return sizeInMm;
+    public long getSize() {
+        return size;
     }
 
-    public void setSizeInMm(long sizeInMm) {
-        this.sizeInMm = sizeInMm;
+    public void setSize(long size) {
+        this.size = size;
     }
 }
