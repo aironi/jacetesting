2e326481b8dc40fab9d4e3448377eccb387c966c
Iiro Hietala <iiro.hietala@descom.fi>
2014-05-30 16:23:00 +0300

    Jace #24: Drill -> Removed encapsulation

--- testing/src/main/java/org/silverduck/jacetesting/domain/tool/Drill.java
+++ testing/src/main/java/org/silverduck/jacetesting/domain/tool/Drill.java
@@ -7,11 +7,5 @@ public class Drill extends AbstractTool {
 
     private String brand;
 
-    public String getBrand() {
-        return brand;
-    }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
 }
