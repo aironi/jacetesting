0a9ec7793d219f50f2d8bd100e8504fd3b5b9ab1
Iiro Hietala <iiro.hietala@descom.fi>
2014-05-30 16:23:18 +0300

    Jace #25: Drill -> Removed extends

--- testing/src/main/java/org/silverduck/jacetesting/domain/tool/Drill.java
+++ testing/src/main/java/org/silverduck/jacetesting/domain/tool/Drill.java
@@ -3,7 +3,7 @@ package org.silverduck.jacetesting.domain.tool;
 /**
  * Created by ihietala on 30.5.2014.
  */
-public class Drill extends AbstractTool {
+public class Drill {
 
     private String brand;
 
