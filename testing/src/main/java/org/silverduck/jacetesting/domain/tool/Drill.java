56f9577695c719c724d4dbd7d2405b1358813032
Iiro Hietala <iiro.hietala@descom.fi>
2014-05-30 17:33:27 +0300

    Jace #27: Drill -> Added socket size

--- testing/src/main/java/org/silverduck/jacetesting/domain/tool/Drill.java
+++ testing/src/main/java/org/silverduck/jacetesting/domain/tool/Drill.java
@@ -3,9 +3,11 @@ package org.silverduck.jacetesting.domain.tool;
 /**
  * Created by ihietala on 30.5.2014.
  */
-public class Drill  extends AbstractTool {
+public class Drill extends AbstractTool {
 
     private String brand;
+
+    private Long socketSize;
 
 
     public String getBrand() {
@@ -14,5 +16,13 @@ public class Drill  extends AbstractTool {
 
     public void setBrand(String brand) {
         this.brand = brand;
+    }
+
+    public Long getSocketSize() {
+        return socketSize;
+    }
+
+    public void setSocketSize(Long socketSize) {
+        this.socketSize = socketSize;
     }
 }
