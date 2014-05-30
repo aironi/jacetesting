680b5ffadfb3b09c0fb7ac16295a62a032ad061c
Iiro Hietala <iiro.hietala@descom.fi>
2014-05-30 13:14:17 +0300

    Jace #21: ScrewDriver: Add Polymorphism

--- testing/src/main/java/org/silverduck/jacetesting/domain/tool/ScrewDriver.java
+++ testing/src/main/java/org/silverduck/jacetesting/domain/tool/ScrewDriver.java
@@ -3,7 +3,8 @@ package org.silverduck.jacetesting.domain.tool;
 /**
  * Created by ihietala on 30.5.2014.
  */
-public class ScrewDriver {
+public class ScrewDriver extends AbstractTool {
     private int size;
+
 
 }
