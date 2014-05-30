9747f4077a05b08d3782106d9f1d1439a0a11565
Iiro Hietala <iiro.hietala@descom.fi>
2014-05-30 19:17:24 +0300

    Jace #28: Added new hammering process

---
+++ testing/src/main/java/org/silverduck/jacetesting/domain/process/HammeringProcess.java
@@ -0,0 +1,15 @@
+package org.silverduck.jacetesting.domain.process;
+
+/**
+ * Created by ihietala on 30.5.2014.
+ */
+public class HammeringProcess {
+
+    public void hammerFiercely() {
+        System.out.println("Hammering in red-eyed fury with the power of thousand warriors...");
+    }
+
+    public void hammerSoftly() {
+        System.out.println("Hammering sofly and neatly..");
+    }
+}
